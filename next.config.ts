0f1a12f023f9a514a4b6df6c3dbf28cca2fe385d
01-Tolibovich <01.tolibovich@gmail.com>
2025-07-15 15:54:25 +0500

    styles

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
+import path from 'path';
+
 const nextConfig = {
   images: {
     domains: ['cdn.dummyjson.com', 'i.dummyjson.com'],
   },
   experimental: {
     appDir: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/app/styles')],
+    prependData: `@import "variables.scss";`,
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import './styles/globals.css';
 import { ClientWrapper, Footer, Header } from '@/components';
 
 export const metadata: Metadata = {
